f3005da4688d03cdb31169ef449f1a0d463ae283
Abdurahmon1006 <abdurahmonk1987@gmail.com>
2026-01-28 00:05:26 +0500

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '8309915799:AAFtCeDgLHzkUsH7EuSA_W06yMiNhpOrdCg',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:BzCefvTESHySBVYxSkpPXIOirYgCEsHt@postgres.railway.internal:5432/railway',
+    DATABASE_URL: process.env.DATABASE_URL || 'https://yscpunritjgqozjjpvwc.supabase.co',
     ADMIN_ID: process.env.ADMIN_ID || 'abd201717121413',
     PORT: process.env.PORT || 3000,
 };
