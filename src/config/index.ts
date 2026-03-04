41a9b3c500d8790ac75de576e417fc42463ec459
Abdurahmon1006 <abdurahmonk1987@gmail.com>
2026-03-04 07:47:45 +0000

    i dont no

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const config = {
-    BOT_TOKEN: process.env.BOT_TOKEN || '',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:abd201717121413@db.yscpunritjgqozjjpvwc.supabase.co:5432/postgres',
-    ADMIN_ID: process.env.ADMIN_ID || '',
+    BOT_TOKEN:
+        process.env.BOT_TOKEN ||
+        "8309915799:AAFtCeDgLHzkUsH7EuSA_W06yMiNhpOrdCg",
+    DATABASE_URL:
+        process.env.DATABASE_URL ||
+        "postgresql://postgres:abd201717121413@db.yscpunritjgqozjjpvwc.supabase.co:5432/postgres",
+    ADMIN_ID: process.env.ADMIN_ID || "",
     PORT: process.env.PORT || 3000,
 };
 
